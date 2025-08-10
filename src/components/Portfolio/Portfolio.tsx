fcbf2ff509a5d47a8bce58e0e8971ce565154e10
Porramat081 <porramat081@gmail.com>
2025-08-10 17:56:53 +0700

    edit:thumnail

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -18,7 +18,7 @@ const Portfolio = () => {
           url1="https://text-counter-iota.vercel.app/"
           url2="https://github.com/Ohm0025/text-counter.git"
           urlApi="https://github.com/Ohm0025/ielts-writing-api.git"
-          img={"public/image/ieltCal.png"}
+          img={"https://pic.in.th/image/Screenshot-2025-08-10-173155.cnUfGC"}
         />
 
         <PortItem
@@ -26,7 +26,7 @@ const Portfolio = () => {
           url1="https://ohello-frontend.vercel.app/"
           url2="https://github.com/Porramat081/ohello-frontend"
           urlApi="https://github.com/Porramat081/ohello-api"
-          img={"public/image/ohmSocia.png"}
+          img={"/image/ohmSocia.png"}
         />
 
         <PortItem
@@ -34,7 +34,7 @@ const Portfolio = () => {
           url1="https://youtu.be/wv3Rl34TUOk?si=Waq7ScHtKm3rruD0"
           url2="https://github.com/Ohm0025/cross-opd-react"
           urlApi="https://github.com/Ohm0025/cross-opd-api"
-          img={"public/image/crossopdthumb.png"}
+          img={"https://img5.pic.in.th/file/secure-sv1/crossopdthumb.png"}
         />
 
         <PortItem
@@ -42,12 +42,12 @@ const Portfolio = () => {
           url1="https://ohmkahnwald.com/"
           url2="https://github.com/Ohm0025/ohmkahnwald-react"
           urlApi="https://github.com/Ohm0025/ohmkahnwald-api"
-          img={"public/image/ohmBlog.png"}
+          img={"https://pic.in.th/image/Screenshot-2025-08-10-161006.cnUmXv"}
         />
 
         <PortItem
           title="Simple todo list"
-          url1=""
+          url1="https://www.youtube.com/watch?v=tpsSFZJZ_VA&ab_channel=OHMBlueeyedragon"
           url2="https://github.com/Ohm0025/simple-todo-list"
           urlApi="https://github.com/Ohm0025/todo-list-api"
           img={"https://img5.pic.in.th/file/secure-sv1/todolistthumb.png"}
@@ -58,3 +58,6 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+// https://pic.in.th/image/Screenshot-2025-08-10-161006.cnUmXv
+// https://pic.in.th/image/Screenshot-2025-08-10-173155.cnUfGC
